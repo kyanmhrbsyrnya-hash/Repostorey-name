4b8216c51ebef16a89b4479d06b4756b810c8650
kyanmhrbsyrnya-hash <kyanmhrbsyrnya@gmail.com>
2026-07-26 13:38:07 +0330

    Create 01_basics_practice.py

---
+++ 01_basics_practice.py
@@ -0,0 +1,17 @@
+
+temp_c = int(input('دما به سانتی‌گراد را وارد کنید: '))
+temp_f = (temp_c * 9/5) + 3 
+print(f'دما {temp_c} سانتی‌گراد برابر است با {temp_f} فارنهایت')
+
+####
+length = float(input('طول مستطیل: '))
+width = float(input('عرض مستطیل: '))
+
+area = length * width
+
+print(f'مساحت مستطیل برابر است با: {area}')
+
+
+
+####
+
