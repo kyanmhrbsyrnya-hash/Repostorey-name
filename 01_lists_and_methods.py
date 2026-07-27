e9d3e28189fa425d65d5cde2b9e8db3f708ceb64
kyanmhrbsyrnya-hash <kyanmhrbsyrnya@gmail.com>
2026-07-27 15:47:41 +0330

    Create 01_lists_and_methods.py

---
+++ 01_lists_and_methods.py
@@ -0,0 +1,35 @@
+fav_sports=['karate','wreasting','sambo']
+if('karate' in fav_sports):
+    print('بله هست')
+    print(len(f'تعداد ورزش ها:{fav_sports}'))
+
+
+####
+fav_sports=['karate','wreasting','sambo']
+if('karate' in fav_sports):
+    print('بله هست')
+    print(len(f'تعداد ورزش ها:{fav_sports}'))
+else:
+    print('نیست')
+
+
+####
+fav_sports=['karate','wreasting','sambo']
+print(len(fav_sports))
+
+
+####
+friend=['فرنام','رادمان','نیکان','اکام','مانی']
+print(friend[0])
+print(friend[-1])
+
+
+####
+friend=['فرنام','رادمان','نیکان','اکام','مانی']
+print(friend[0])
+print(friend[-1])
+print(len(friend))
+if('عباس' in friend):
+    print('وجود دارد')
+else:
+    print('وجود ندارد')
