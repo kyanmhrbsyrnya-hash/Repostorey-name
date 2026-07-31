e69f27bed26427970e365b9ddd1c0d87bdae24ed
kyanmhrbsyrnya-hash <kyanmhrbsyrnya@gmail.com>
2026-07-31 18:57:17 +0330

    تمرینی

---
+++ for_practice.py
@@ -0,0 +1,28 @@
+####جمع اعداد لیست با۲
+num_list=[5,10,15,20]
+for num in num_list:
+    print(num+2)
+
+
+
+####نوشتن تمام اسامی رفقا
+friends_list=['علی','فرنام','حسنی','نیکان']
+for friends in friends_list
+print(friends)
+
+
+
+####
+num_list=[5,10,15,20]
+total=(0)
+for num in num_list:
+    print(num+total)
+
+
+####
+numbers_list = [2, 15, 8, 22, 30, 4]
+filtereds_list = []
+for num in numbers_list:
+    if num > 10:
+        filtereds_list.append(num)
+print(filtereds_list)
