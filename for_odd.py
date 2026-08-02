4f2602d98b42c979a8debba9f3e772c26473cfc2
kyanmhrbsyrnya-hash <kyanmhrbsyrnya@gmail.com>
2026-08-02 19:48:38 +0330

    Create for_odd.py

---
+++ for_odd.py
@@ -0,0 +1,22 @@
+num_lists=range(2,20,2)
+for num in num_lists:
+    print(num)
+
+
+####جدول ضرب۵
+num_lists=range(1,10,1)
+for num in num_lists:
+    print(num*5)
+
+
+####معکوس نویسی به سبک اسطوره
+a = 1
+b = 10
+for num in range(1, 11):
+    print(f"Current number: {num} | a: {a}, b: {b}")
+    a, b = b, a  # اینجا جابه‌جایی اتفاق می‌افتد
+
+
+
+
+####
