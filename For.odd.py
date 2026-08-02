49bc050804a76fb2d9bd0e586a224d0726a4adc2
kyanmhrbsyrnya-hash <kyanmhrbsyrnya@gmail.com>
2026-08-02 19:51:51 +0330

    Create For.odd.py

---
+++ For.odd.py
@@ -0,0 +1,31 @@
+a = 1
+b = 10
+for num in range(1, 11):
+    print(f"Current number: {num} | a: {a}, b: {b}")
+    a, b = b, a  # اینجا جابه‌جایی اتفاق می‌افتد
+
+
+
+###
+num_lists=range(1,10,1)
+for num in num_lists:
+    print(num*5)
+
+
+
+####
+num_lists=range(1,20,1)=(a,b,1)
+for num in num_lists:
+    print(num)
+    a,b=b,a
+
+
+
+####
+num_lists=range(2,20,2)
+for num in num_lists:
+    print(num)
+
+
+
+####
