2b01eb198980cff578eec514e94ad2e92b58590d
kyanmhrbsyrnya-hash <kyanmhrbsyrnya@gmail.com>
2026-08-02 21:38:41 +0330

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,18 @@
+fruit_lists=['banana','mango','apple','coconat','orange']
+fruit_lists[1]='cherry'
+print(fruit_lists)
+
+
+
+####
+fruit_lists=['banana','mango','apple','coconat','orange']
+fruit_lists[1]='cherry'
+print(fruit_lists)
+print(len(fruit_lists))
+
+
+
+####
+scores = [18, 15, 20, 19, 12, 17]
+student_info = [15, 170, 75]
+print(scores+student_info)
