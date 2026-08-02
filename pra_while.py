162794e3a906989ad77b6ddb8efb54ae1ca4f021
kyanmhrbsyrnya-hash <kyanmhrbsyrnya@gmail.com>
2026-08-02 19:22:06 +0330

    Create pra_while.py

---
+++ pra_while.py
@@ -0,0 +1,32 @@
+a = 5
+b = 10
+a, b = b, a
+print(a, b)
+
+
+
+####شمارش۱تا۱۰۰
+num=1
+while(num<=100):
+    print(num)
+    num=num+1
+
+
+
+####شمارنده های ۲
+num=2
+while(num<100):
+    print(num)
+    num=num*2
+
+
+
+####چاپ کردن خودکار اسم۵بار
+nam=1
+while(nam<=5):
+    print('kianmehr')
+    nam=nam+1
+
+
+
+####
